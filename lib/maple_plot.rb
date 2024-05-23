8aae4ee32552ad8dae2ea775ad02d85552fb3e58
Shigeto R. Nishiani <daddygongon@users.noreply.github.com>
2024-05-23 13:45:07 +0900

    add listplot3d

--- lib/maple_plot.rb
+++ lib/maple_plot.rb
@@ -4,6 +4,25 @@ require_relative "maple_plot/version"
 
 module MaplePlot
   class Error < StandardError; end
-  puts "Hello world."
-  
+  def hello
+    "Hello world."
+  end
+  def listplot3d(data_name='data')
+    
+    cont =<<"EOS"
+with(plots):
+listplot3d(#{data_name},
+axesfont=[Helvetica,   roman, 14],
+#legend = ["Sine Plot", "Cosine Plot"], 
+titlefont = ["ARIAL", 15], 
+#title = "TITLE",
+labels = ["x", "y","boundary energy [mJ/m^2]"], 
+labeldirections = ["horizontal", "horizontal","vertical"], 
+labelfont = [Helvetica, 14]
+#linestyle = [solid, longdash], 
+#legendstyle = [font = [
+#"HELVETICA", 9], location = right]
+);
+EOS
+  end
 end
